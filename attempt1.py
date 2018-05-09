38cd0ad879bf5d9adaf3bdd5a3601246bc200b2e
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-05-09 11:17:47 -0500

    just set up some stuff

---
+++ attempt1.py
@@ -0,0 +1,13 @@
+import RoboPiLib as RPL
+import setup
+import time
+#so for this one, I was thinking that we could make the robot turn based on...
+#...direction from the magnetometer
+
+#setting up the motor pins
+mL = 0
+mR = 2
+
+#setting up the motor speeds
+spl = 2000
+spr = 100
